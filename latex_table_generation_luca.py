254a9020d56ad0a3fe579ecca428edf9c7f9a826
Luca <luca.herrtti@gmail.com>
2024-10-29 16:35:47 +0100

    minor changes

--- latex_table_generation_luca.py
+++ latex_table_generation_luca.py
@@ -1,4 +1,8 @@
 import os, sys
+import warnings
+from tqdm import tqdm
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 CDIR = os.path.dirname(os.path.realpath(__file__))
 WORKDIR = os.path.abspath(os.path.join(CDIR, '..'))
@@ -8,25 +12,25 @@ sys.path.append(WORKDIR)
 import pandas as pd
 from private_obfuscation.paths import PODATADIR
 
-metrics_oi_ = ['mean_bert_similarity', 'mean_jaccard_similarity', 'mean_szymkiewicz_similarity', 'map', 'nDCG@10', 'nDCG@100', 'P@10', 'P@100']
+metrics_oi_ = ['mean_bert_similarity', 'mean_jaccard_similarity', 'mean_szymkiewicz_similarity', 'map', 'nDCG@10',
+               'nDCG@100', 'P@10', 'P@100']
 # datasets_oi_ = ['irds:msmarco-document/trec-dl-2020', 'irds:beir/nfcorpus/test']
 
 # load csv
 pandas_path = os.path.join(PODATADIR, 'results.csv')
 df = pd.read_csv(pandas_path)
-print(df.head())
+
+# reduce precision of metrics to :.2f
+df = df.round(3)
 
 # select only retriever BM25
 df = df[df['retriever'] == 'BM25']
-print(df.head())
-print(list(df.columns))
 
 all_reformulations = df['reformulation'].unique()
 all_datasets = df['dataset_name'].unique()
 
 print("', '".join(all_reformulations))
 print(all_datasets)
-
 
 # new columns: reformulation,
 
@@ -51,26 +55,64 @@ refs = [
 ]
 
 
+def bold_and_underline(table, metrics_oi, prefix_metric=''):
+    pm = prefix_metric
+
+    # split the table between rows to skip and those to treat
+
+    for m in metrics_oi:
+        # in bold
+        # table[f'{pm}{m}'] = table[f'{pm}{m}'].apply(lambda x: x.replace('\%', '')).astype(float)
+
+        # get best and second best
+        table[f'{pm}{m}'] = table[f'{pm}{m}'].astype(float)
+        best_metric = table[f'{pm}{m}'].max()
+        second_best_metric = table[f'{pm}{m}'].sort_values(ascending=False).iloc[1]
+
+        # in bold
+        table[f'{pm}{m}'] = table[f'{pm}{m}'].apply(
+            lambda x: f'\\textbf{{{x}}}' if str(x) == str(best_metric) else x
+        )
+        # underlined
+        table[f'{pm}{m}'] = table[f'{pm}{m}'].apply(
+            lambda x: f'\\underline{{{x}}}' if str(x) == str(second_best_metric) else x
+        )
+
+        # table[f'{pm}{m}'] = table[f'{pm}{m}'].apply(lambda x: x)
+
+    # sort by index
+    # table = table.sort_index()
+
+    return table
+
+
 def create_table(df, refs, datasets_oi, metrics_oi):
+    sdf = df.copy()
+
     rows = []
-    row_0 = [''] + [f'd{i}' for i, dataset in enumerate(datasets_oi) for j, metric in enumerate(metrics_oi)]
+    row_0 = [''] + [f'd{i}' for i, dataset in enumerate(datasets_oi) for _, _ in enumerate(metrics_oi)]
     rows.append(row_0)
-    row_1 = ['reformulator'] + [metric for dataset in datasets_oi for metric in metrics_oi]
+    row_1 = ['reformulator'] + [metric for _ in datasets_oi for metric in metrics_oi]
     rows.append(row_1)
 
     for ref in refs:
         row = [ref]
         for dataset in datasets_oi:
+            dsdf = sdf[(sdf['dataset_name'] == dataset)]
+            dsdf = bold_and_underline(dsdf, metrics_oi)
+            print(dsdf.head())
+
             for metric in metrics_oi:
-                mask = (df['dataset_name'] == dataset) & (df['reformulation'] == ref)
-                row.append(f'{df[mask][metric].mean():.3f}')
+                mask = (dsdf['reformulation'] == ref)
+                print(ref, dataset, metric)
+                print(dsdf[mask][metric].values[0])
+                row.append(f'{dsdf[mask][metric].values[0]}')
         rows.append(row)
 
-
     # to pandas and then to latex
-    df = pd.DataFrame(rows)
+    sdf = pd.DataFrame(rows)
 
-    latex = df.to_latex(index=False, header=False)
+    latex = sdf.to_latex(index=False, header=False)
 
     latex = f"""
 
@@ -109,15 +151,20 @@ def create_table(df, refs, datasets_oi, metrics_oi):
     latex = latex.replace('mean_jaccard_similarity', 'Jacc')
     latex = latex.replace('mean_szymkiewicz_similarity', 'Szym')
     latex = latex.replace('map', 'MAP')
-    latex = latex.replace('}0', '0}').replace( 'vik\\textbf{', '\\textbf{vik')
-
-
+    latex = latex.replace('}0', '0}').replace('vik\\textbf{', '\\textbf{vik')
 
     for i in [1, 5, 50]:
         latex = latex.replace(f'_e{i}', f' $\\epsilon = {i}$')
 
-    print(latex)
+    latex = latex.replace('$0', '0$').replace('nan', '-')
+
+    return latex
 
 
-for dataset in all_datasets:
-    create_table(df, refs, [dataset], metrics_oi_)
+all_latexes = ''
+for dataset in tqdm(all_datasets):
+    print(f"Generating table for {dataset}")
+    latex = create_table(df.copy(), refs, [dataset], metrics_oi_)
+    all_latexes += '\n\n' + latex
+
+print(all_latexes)
